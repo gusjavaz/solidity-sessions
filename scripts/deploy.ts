15644a5e6644a9ac5521dd652c82790124a1427e
gusjavaz <gusjavaz@gmail.com>
2023-03-22 15:33:53 +0100

    Created test contract

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,14 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const initialValue = 100;
+  const storageFactory = await ethers.getContractFactory("Storage");
+  const storage = await storageFactory.deploy(initialValue);
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
+  await storage.deployed();
 
   console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    `Storage with initial value=${initialValue} deployed to ${storage.address}`
   );
 }
 
